42bc666f54c90df098ed85ce4f4edcaa46ccaea7
shun91 <shun9167@gmail.com>
2019-12-07 23:12:55 +0900

    ♻️ nuxt.config.js -> ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
+import { Configuration } from '@nuxt/types'
 
-export default {
+const config: Configuration = {
   mode: 'universal',
   /*
    ** Headers of the page
@@ -70,3 +71,5 @@ export default {
     extend(config, ctx) {}
   }
 }
+
+export default config
